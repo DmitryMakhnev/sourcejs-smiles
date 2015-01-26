28049f6d8657d04cced4e9653fa68257ea2e77de
Dmitry Makhnev <dmitry.ivshin@gmail.com>
2015-01-26 16:29:05 +0300

    v0.0.2 fix check plugin options

--- core/middleware/sourcejs-smiles/index.js
+++ core/middleware/sourcejs-smiles/index.js
@@ -1,20 +1,100 @@
 var util = require('util');
 
+var options = opts;
+var logger = log;
+
+/**
+ * get object value safely
+ * @param {Object|Null|undefined} object
+ * @param {String|Number|Array} [key] object proterties keys
+ * @returns {*} result || null
+ */
+function getValueByObjectKeys(object, key){
+    var keyTypeof = typeof key,
+        i,
+        iMax,
+        keysList,
+        cache,
+        result,
+        u;
+    if (object){
+        if ((keyTypeof !== 'string')
+            && (keyTypeof !== 'number')){
+            keysList = key;
+            i = 0;
+        } else{
+            keysList = arguments;
+            i = 1;
+        }
+        iMax = keysList.length - 1;
+        for (; i < iMax; i += 1){
+            cache = object[keysList[i]];
+            if (cache === u){
+                return null;
+            } else{
+                object = cache;
+            }
+        }
+        result = object[keysList[i]];
+        if (result !== u){
+            return result;
+        }
+    }
+    return null;
+};
+
+
+/**
+ *
+ * @param {Object} request Express request object
+ * @return {}
+ */
+function isNotIgnoreFile (request) {
+    var ignoreURLs = getValueByObjectKeys(
+            options,
+            'assets',
+            'pluginsOptions',
+            'smiles',
+            'ignoreURLs'
+        );
+
+    var path = request.path;
+    var index = path.indexOf('/index.src');
+    if (index !== -1) {
+        path = path.substr(0, index + 1);
+    }
+
+    return !(ignoreURLs
+            && (ignoreURLs.indexOf(path) !== -1));
+}
+
+/**
+ *
+ * @param {Object} request Express request object
+ * @return {Boolean}
+ */
+function isNeedProcessing(request) {
+    var specData = request.specData;
+    logger.info({isIgnore: isNotIgnoreFile(request), path: request.path});
+    return isNotIgnoreFile(request)
+        && specData
+        && specData.renderedHtml;
+}
+
 /*
- * Получаем html из реквеста и заменяем смайлики на соответствующие картинки
+ * Get pre-final HTML from request, and replace smiles to images
  *
  * @param {object} req - Request object
  * @param {object} res - Response object
  * @param {function} next - The callback function
  * */
 exports.process = function (req, res, next) {
-    if ((global.opts.assets.pluginsOptions.smiles.ignoreURLs.indexOf(req.path) !== -1)
-        && req.specData
-        && req.specData.renderedHtml) {
+
+    if (isNeedProcessing(req)) {
         var html = req.specData.renderedHtml;
 
         /* some manipulations */
-        var smileTpl = "<img src='/source/assets/i/smiles/%s.png' alt='%s' />";
+        var smileTpl = "<img src='/node_modules/sourcejs-smiles/assets/i/smiles/%s.png' alt='%s' />";
 
         html = html.replace(/\:\)/g, util.format(smileTpl, "1f600", ":)"));
         html = html.replace(/\:D/g, util.format(smileTpl, "1f602", ":D"));
